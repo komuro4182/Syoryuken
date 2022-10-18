fb2e9085334294b3ed62edf04c8efb5b2cef389f
komuro4182 <komuro4182@gmail.com>
2022-10-18 21:55:44 +0900

    SYORYUKEN完成

--- Syoryuken/ViewController.swift
+++ Syoryuken/ViewController.swift
@@ -8,12 +8,29 @@
 import UIKit
 
 class ViewController: UIViewController {
+    var imageArraySyoryuken : Array<UIImage> = []
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        while let syoryukenImageView = UIImage(named: "attak\(imageArraySyoryuken.count+1)") {
+                    imageArraySyoryuken.append(syoryukenImageView)
+            
+                }
+            
         // Do any additional setup after loading the view.
     }
+    
+    @IBOutlet weak var syoryukenImageView: UIImageView!
+    
+    
+    
+    @IBAction func btn(_ sender: Any) {
+        syoryukenImageView.animationImages = imageArraySyoryuken
+               syoryukenImageView.animationDuration = 1.5
+        syoryukenImageView.animationRepeatCount = 1
+        syoryukenImageView.startAnimating()
+           }
 
 
 }
-
